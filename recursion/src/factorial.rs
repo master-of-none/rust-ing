1f45c1621b539786eeda3eec1d62b349f3906bd7
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-15 09:47:19 -0700

    tail recursion

--- recursion/src/factorial.rs
+++ recursion/src/factorial.rs
@@ -1,11 +1,12 @@
 pub fn factorial(n: i32) -> i32 {
-    if n == 1 {
-        1
-    } else if n == 0 {
-        1
-    } else {
-        n * factorial(n - 1)
+    fn factorial_helper(n: i32, acc: i32) -> i32 {
+        if n == 0 {
+            acc
+        } else {
+            factorial_helper(n - 1, acc * n)
+        }
     }
+    factorial_helper(n, 1)
 }
 
 #[cfg(test)]
