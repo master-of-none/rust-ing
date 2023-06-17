62d7760181ceef71c5f8f7df95041949069534cd
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-17 15:31:06 -0700

    added tests

--- echo_rust/tests/cli.rs
+++ echo_rust/tests/cli.rs
@@ -2,6 +2,7 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
+const OUTFILE: &str = "tests/expected/";
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
@@ -14,31 +15,69 @@ fn dies_no_args() -> TestResult {
     Ok(())
 }
 
-#[test]
-fn runs() -> TestResult {
-    let mut cmd = Command::cargo_bin("echo_rust")?;
-    cmd.arg("hello").assert().success();
+// #[test]
+// fn runs() -> TestResult {
+//     let mut cmd = Command::cargo_bin("echo_rust")?;
+//     cmd.arg("hello").assert().success();
+//     Ok(())
+// }
+
+// #[test]
+// fn hello1() -> TestResult {
+//     let outfile = "tests/expected/hello1.txt";
+//     let expected = fs::read_to_string(outfile)?;
+//     let mut cmd = Command::cargo_bin("echo_rust")?;
+//     cmd.arg("Hello there").assert().success().stdout(expected);
+//     Ok(())
+// }
+
+// #[test]
+// fn hello2() -> TestResult {
+//     let outfile = "tests/expected/hello2.txt";
+//     let expected = fs::read_to_string(outfile)?;
+//     let mut cmd = Command::cargo_bin("echo_rust")?;
+
+//     cmd.args(vec!["Hello", "there"])
+//         .assert()
+//         .success()
+//         .stdout(expected);
+//     Ok(())
+// }
+
+// Since there are 4 files, must write hepler function and pass files
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echo_rust")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
     Ok(())
 }
 
 #[test]
 fn hello1() -> TestResult {
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile)?;
-    let mut cmd = Command::cargo_bin("echo_rust")?;
-    cmd.arg("Hello there").assert().success().stdout(expected);
-    Ok(())
+    run(&["Hello there"], &(format!("{}hello1.txt", OUTFILE)))
 }
 
 #[test]
 fn hello2() -> TestResult {
-    let outfile = "tests/expected/hello2.txt";
-    let expected = fs::read_to_string(outfile)?;
-    let mut cmd = Command::cargo_bin("echo_rust")?;
+    run(&["Hello", "there"], &(format!("{}hello2.txt", OUTFILE)))
+}
 
-    cmd.args(vec!["Hello", "there"])
-        .assert()
-        .success()
-        .stdout(expected);
-    Ok(())
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(
+        &["Hello there", "-n"],
+        &(format!("{}hello1.n.txt", OUTFILE)),
+    )
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(
+        &["-n", "Hello", "there"],
+        &(format!("{}hello2.n.txt", OUTFILE)),
+    )
 }
