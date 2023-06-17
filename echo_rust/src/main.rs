b8248df388aa21ca7227d0296b9850daead3579a
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-16 22:30:19 -0700

    used crate clap

--- echo_rust/src/main.rs
+++ echo_rust/src/main.rs
@@ -1,3 +1,10 @@
+use clap::App;
+
 fn main() {
-    println!("{:#?}", std::env::args());
+    //println!("{:#?}", std::env::args());
+    let _matches = App::new("echo_rust")
+        .version("0.1.0")
+        .author("master-of-none <shrikrishna.bht@gmail.com>")
+        .about("Rust echo comand")
+        .get_matches();
 }
