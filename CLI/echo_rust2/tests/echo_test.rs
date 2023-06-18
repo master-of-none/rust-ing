35d559a88028d7229fe50ad50ae73caa53353c9a
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-17 23:06:46 -0700

    checked all cases

--- CLI/echo_rust2/tests/echo_test.rs
+++ CLI/echo_rust2/tests/echo_test.rs
@@ -15,3 +15,33 @@ fn hello1() {
     let mut cmd = Command::cargo_bin("echo_rust2").unwrap();
     cmd.arg("Hello there").assert().success().stdout(expected);
 }
+
+#[test]
+fn hello2() {
+    let expected = "Hello there";
+    let mut cmd = Command::cargo_bin("echo_rust2").unwrap();
+    cmd.args(vec!["-n", "Hello", "there"])
+        .assert()
+        .success()
+        .stdout(expected);
+}
+
+#[test]
+fn hello3() {
+    let expected = "Hello there";
+    let mut cmd = Command::cargo_bin("echo_rust2").unwrap();
+    cmd.args(vec!["Hello", "there", "-n"])
+        .assert()
+        .success()
+        .stdout(expected);
+}
+
+#[test]
+fn hello4() {
+    let expected = "Hello -n there -n";
+    let mut cmd = Command::cargo_bin("echo_rust2").unwrap();
+    cmd.args(vec!["-n", "Hello", "-n", "there", "-n"])
+        .assert()
+        .success()
+        .stdout(expected);
+}
