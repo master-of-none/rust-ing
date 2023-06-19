d9c5848f88a0bb3e3afb2bfa7d91159e845c7d85
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-19 16:41:57 -0700

    added taking cmd args

--- CLI/cat_fishing/src/main.rs
+++ CLI/cat_fishing/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if let Err(e) = cat_fishing::run() {
+    if let Err(e) = cat_fishing::get_args().and_then(cat_fishing::run) {
         eprintln!("{}", e);
         std::process::exit(1);
     }
