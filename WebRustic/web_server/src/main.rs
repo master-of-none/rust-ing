558088c28ada416d8d15f605d785992de96b9ee5
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-25 19:52:31 +0530

    hello rust web

--- WebRustic/web_server/src/main.rs
+++ WebRustic/web_server/src/main.rs
@@ -1,11 +1,18 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
 use warp::Filter;
 
 #[tokio::main]
 
 async fn main() {
-    let hello = warp::path("hello")
-        .and(warp::path::param())
-        .map(|name: String| format!("Hello, {}", name));
+    // let hello = warp::path("hello")
+    //     .and(warp::path::param())
+    //     .map(|name: String| format!("Hello, {}", name));
 
-    warp::serve(hello).run(([127, 0, 0, 1], 1337)).await;
+    // warp::serve(hello).run(([127, 0, 0, 1], 1337)).await;
+
+    let hello = warp::get().map(|| format!("Hello World"));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
