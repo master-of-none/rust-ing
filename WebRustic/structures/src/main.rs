74f0ccc1d1a54a867826ff76a5bee2add96069d6
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-25 12:49:03 +0530

    created structure fir book

--- WebRustic/structures/src/main.rs
+++ WebRustic/structures/src/main.rs
@@ -1,3 +1,23 @@
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
