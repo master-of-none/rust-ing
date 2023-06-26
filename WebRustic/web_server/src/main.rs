615d0222454f9e76d1c9a010d792ad56f886cfa6
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-26 11:54:04 +0530

    next steps in creating server

--- WebRustic/web_server/src/main.rs
+++ WebRustic/web_server/src/main.rs
@@ -3,8 +3,50 @@ use std::str::FromStr;
 
 use warp::Filter;
 
-#[tokio::main]
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
 
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No ID given")),
+        }
+    }
+}
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id given"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+
+    Ok(warp::reply::json(&question))
+}
+
+#[tokio::main]
 async fn main() {
     // let hello = warp::path("hello")
     //     .and(warp::path::param())
@@ -17,5 +59,11 @@ async fn main() {
     //! The below code create hellow world route
     let hello = warp::path("hello").map(|| format!("Hello World Route"));
 
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions);
+
+    let routes = get_items;
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
