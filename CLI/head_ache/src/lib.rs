d35aa6fe14978a98aa16a338a8049f90fc03f615
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-07-23 12:39:55 +0530

    added clap code

--- CLI/head_ache/src/lib.rs
+++ CLI/head_ache/src/lib.rs
@@ -9,3 +9,46 @@ pub struct Config {
     lines: usize,
     bytes: Option<usize>,
 }
+
+// Getting Command line args
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("head_ache")
+        .version("0.1.0")
+        .author("Shrikrishna Bhat")
+        .about("Rusty head")
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .default_value("10"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .takes_value(true)
+                .conflicts_with("lines")
+                .help("Number of bytes"),
+        )
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .get_matches();
+
+    let lines = matches.value_of("lines");
+
+    let bytes = matches.value_of("bytes");
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        lines: lines.unwrap(),
+        bytes,
+    })
+}
