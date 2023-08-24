14d125073493aa024b9bc6270d19b679b242eacd
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-24 20:25:25 +0530

    cannot do this

--- fun/lists/src/first.rs
+++ fun/lists/src/first.rs
@@ -18,4 +18,11 @@ impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
     }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Node {
+            elem: elem,
+            next: self.head,
+        };
+    }
 }
