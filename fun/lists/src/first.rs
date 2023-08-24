286d3181c30b20626aba94c053342ef5c0d20532
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-24 20:16:02 +0530

    changed files

--- fun/lists/src/first.rs
+++ fun/lists/src/first.rs
@@ -1,11 +1,15 @@
 // First Linked List
 
-struct Node {
-    elem: i32,
-    next: List,
+pub struct List {
+    head: Link,
 }
 
-pub enum List {
+enum Link {
     Empty,
     More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
 }
