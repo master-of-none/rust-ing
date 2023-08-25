87d9fcf49ff79515faaf36dce369964d76ece820
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-25 21:59:29 +0530

    errors

--- fun/lists/src/first.rs
+++ fun/lists/src/first.rs
@@ -40,9 +40,29 @@ impl List {
     }
 }
 
+#[cfg(test)]
 mod test {
+    use super::List;
     #[test]
-    fn do_nothing() {
-        // TODO
+    fn do_testing() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        aseert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        aseert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        aseert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
     }
 }
