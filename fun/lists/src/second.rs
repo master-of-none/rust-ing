6f58c90ac01bd2610b07390fd9d60457bc6fe3e3
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-26 18:36:47 +0530

    changed according to new rust version

--- fun/lists/src/second.rs
+++ fun/lists/src/second.rs
@@ -30,6 +30,10 @@ impl<T> List<T> {
             node.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
 }
 
 impl<T> Default for List<T> {
