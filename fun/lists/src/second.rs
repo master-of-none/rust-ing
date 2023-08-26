1781953f4a0fce8245bb1d2d13f64b4a77eec210
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-26 19:43:21 +0530

    added iterator

--- fun/lists/src/second.rs
+++ fun/lists/src/second.rs
@@ -1,5 +1,4 @@
 // First Linked List
-
 pub struct List<T> {
     head: Link<T>,
 }
@@ -56,6 +55,23 @@ impl<T> Drop for List<T> {
     }
 }
 
+// Iterators
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
@@ -98,5 +114,21 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
