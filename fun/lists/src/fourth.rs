a4f7ff1596e41e6d9fd76165e0a67325da051116
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-27 22:03:29 +0530

    added constructors

--- fun/lists/src/fourth.rs
+++ fun/lists/src/fourth.rs
@@ -13,3 +13,28 @@ struct Node<T> {
     next: Link<T>,
     prev: Link<T>,
 }
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem,
+            prev: None,
+            next: None,
+        }))
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: None,
+        }
+    }
+}
+
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
