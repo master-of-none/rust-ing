e52662af02dcde23832f23fa7e18ff358a3d868b
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-27 11:42:21 +0530

    added prepend

--- fun/lists/src/third.rs
+++ fun/lists/src/third.rs
@@ -1,3 +1,5 @@
+// Persistent Stack
+
 use std::rc::Rc;
 
 pub struct List<T> {
@@ -9,4 +11,19 @@ type Link<T> = Option<Rc<Node<T>>>;
 struct Node<T> {
     elem: T,
     next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn prepend(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem: elem,
+                next: self.head.clone(),
+            })),
+        }
+    }
 }
