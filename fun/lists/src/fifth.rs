653290bb4cd7034644d950d29a5dfd0f91397c41
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-28 22:35:24 +0530

    added push

--- fun/lists/src/fifth.rs
+++ fun/lists/src/fifth.rs
@@ -1,3 +1,5 @@
+use std::ptr;
+
 pub struct List<T> {
     head: Link<T>,
     tail: *mut Node<T>,
@@ -14,38 +16,37 @@ impl<T> List<T> {
     pub fn new() -> Self {
         List {
             head: None,
-            tail: None,
+            tail: ptr::null_mut(),
         }
     }
 
     pub fn push(&mut self, elem: T) {
-        let new_tail = Box::new(Node { elem, next: None });
+        let mut new_tail = Box::new(Node { elem, next: None });
 
-        let new_tail = match self.tail.take() {
-            Some(old_tail) => {
-                old_tail.next = Some(new_tail);
-                old_tail.next.as_deref_mut()
+        let raw_tail: *mut _ = &mut *new_tail;
+
+        if !self.tail.is_null() {
+            unsafe {
+                (*self.tail).next = Some(new_tail);
             }
-            None => {
-                self.head = Some(new_tail);
-                self.head.as_deref_mut()
-            }
-        };
-        self.tail = new_tail;
+        } else {
+            self.head = Some(new_tail);
+        }
+        self.tail = raw_tail;
     }
 
-    pub fn pop(&mut self) -> Option<T> {
-        self.head.take().map(|head| {
-            let head = *head;
-            self.head = head.next;
+    // pub fn pop(&mut self) -> Option<T> {
+    //     self.head.take().map(|head| {
+    //         let head = *head;
+    //         self.head = head.next;
 
-            if self.head.is_none() {
-                self.tail = None;
-            }
+    //         if self.head.is_none() {
+    //             self.tail = None;
+    //         }
 
-            head.elem
-        })
-    }
+    //         head.elem
+    //     })
+    // }
 }
 
 #[cfg(test)]
