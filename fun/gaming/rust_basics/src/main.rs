ca61a3748901002d2f86999e5ab571b6ee7dcbc3
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-29 11:43:32 +0530

    added arrays

--- fun/gaming/rust_basics/src/main.rs
+++ fun/gaming/rust_basics/src/main.rs
@@ -7,7 +7,21 @@ fn main() {
 
     let name = enter_name();
 
-    println!("Glad to have you, {}", name);
+    let visitor_list = ["bert", "steve", "fred", "kishan"];
+
+    let mut allowed = false;
+
+    for visitor in &visitor_list {
+        if visitor == &name {
+            allowed = true;
+        }
+    }
+
+    if allowed {
+        println!("Glad to you have you {}", name)
+    } else {
+        println!("Sorry, You are not in the visitor list")
+    }
 }
 
 fn enter_name() -> String {
