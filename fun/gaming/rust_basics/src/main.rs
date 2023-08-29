9ca31117e6c216be6daa927841fd6e624ce38353
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-29 10:56:02 +0530

    added reading input from stdin

--- fun/gaming/rust_basics/src/main.rs
+++ fun/gaming/rust_basics/src/main.rs
@@ -1,3 +1,12 @@
+// TreeHouse
+
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome, What is your name?");
+
+    let mut name = String::new();
+    stdin().read_line(&mut name).expect("Cannot read line");
+
+    println!("Glad to have you, {}", name);
 }
