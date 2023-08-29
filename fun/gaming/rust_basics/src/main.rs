09b2d1efaf6f5f3181ad7165603c8db4fc05c489
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-29 12:19:58 +0530

    match and iterators

--- fun/gaming/rust_basics/src/main.rs
+++ fun/gaming/rust_basics/src/main.rs
@@ -2,25 +2,39 @@
 
 use std::io::stdin;
 
-fn main() {
-    println!("Welcome, What is your name?");
+struct Visitor {
+    name: String,
+    greeting: String,
+}
 
-    let name = enter_name();
-
-    let visitor_list = ["bert", "steve", "fred", "kishan"];
-
-    let mut allowed = false;
-
-    for visitor in &visitor_list {
-        if visitor == &name {
-            allowed = true;
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
         }
     }
 
-    if allowed {
-        println!("Glad to you have you {}", name)
-    } else {
-        println!("Sorry, You are not in the visitor list")
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+fn main() {
+    println!("Welcome, What is your name?");
+    let visitors = [
+        Visitor::new("kishan", "Hello Kishan enjoy"),
+        Visitor::new("LUFFY", "Kaizoku niro otakotoa"),
+        Visitor::new("NaRuTo", "Hokage, Dattebaayo"),
+    ];
+
+    println!("Enter your name");
+    let name = enter_name();
+
+    let allowed = visitors.iter().find(|visitor| visitor.name == name);
+
+    match allowed {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("Not allowed exit"),
     }
 }
 
