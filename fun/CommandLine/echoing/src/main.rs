e24be8bee801905694a0a9de6cca0db469d2c27b
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-01-12 16:47:50 -0800

    updated matches

--- fun/CommandLine/echoing/src/main.rs
+++ fun/CommandLine/echoing/src/main.rs
@@ -1,9 +1,24 @@
-use clap::Command;
+use clap::{Arg, Command};
 
 fn main() {
-    let _matches = Command::new("echor")
+    let matches = Command::new("echor")
         .version("0.1.0")
         .author("Shrikrishna Bhat")
         .about("Rust Echo")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input Text")
+                .required(true)
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .help("Do not print newline")
+                .num_args(0),
+        )
         .get_matches();
+
+    println!("{:#?}", matches)
 }
