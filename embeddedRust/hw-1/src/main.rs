6d430ac5e3717088ee1a753b69246887871a3879
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-01-26 11:14:45 -0800

    Random generator

--- embeddedRust/hw-1/src/main.rs
+++ embeddedRust/hw-1/src/main.rs
@@ -1,41 +1,62 @@
 #![no_std]
 #![no_main]
 
+mod life;
+use life::*;
+
+use nanorand::Rng;
+
 use cortex_m_rt::entry;
-use microbit::{
-    board::Board,
-    hal::{prelude::*, timer::Timer},
-};
+use microbit::{board::Board, display::blocking::Display, hal::timer::Timer};
 
+//use core::time::Duration;
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
-
-enum State {
-    LedOn,
-    LedOff,
-}
+use rtt_target::{rprint, rprintln, rtt_init_print};
 
 #[entry]
 fn init() -> ! {
     rtt_init_print!();
-    let mut board = Board::take().unwrap();
+    let board = Board::take().unwrap();
+    let mut display = Display::new(board.display_pins);
+    let mut timer = Timer::new(board.TIMER0);
 
-    board.display_pins.row1.set_high().unwrap();
+    // Game of Life board
+    // let mut life_board = [
+    //     [0, 0, 0, 0, 0],
+    //     [0, 0, 1, 0, 0],
+    //     [0, 1, 1, 1, 0],
+    //     [0, 0, 1, 0, 0],
+    //     [0, 0, 0, 0, 0],
+    // ];
+    let mut life_board = [[0; 5]; 5];
+    generate_random_board(&mut life_board);
 
-    let mut led1 = board.display_pins.col1;
-    let mut led2 = board.display_pins.col2;
-
-    loop {
-        if let Ok(true) = board.buttons.button_a.is_high() {
-            let _ = led1.set_high();
-        } else {
-            let _ = led1.set_low();
+    for r in 0..5 {
+        for c in 0..5 {
+            rprint!("{}", life_board[r][c]);
         }
+        rprintln!();
+    }
+    loop {
+        display.show(&mut timer, life_board, 1000);
+        life(&mut life_board);
+        if done(&life_board) {
+            break;
+        }
+    }
+    panic!("Done")
+}
 
-        if let Ok(true) = board.buttons.button_b.is_high() {
-            let _ = led2.set_high();
-        } else {
-            let _ = led2.set_low();
+fn generate_random_board(life_board: &mut [[u8; 5]]) {
+    // let current_time = Duration::from_secs(2);
+    // let seed = current_time.as_secs();
+    // let mut rng = nanorand::Pcg64::new_seed(seed.into());
+    let mut rng = nanorand::Pcg64::new_seed(10);
+    for r in 0..5 {
+        for c in 0..5 {
+            let b: bool = rng.generate();
+            // life_board[r][c] = rng.generate_range(0..2);
+            life_board[r][c] = b as u8;
         }
     }
 }
