b7944e14c1e3ee1a85167663f23b90534527a937
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-01-28 20:33:13 -0800

    Function to handle A and B buttons ✅

--- embeddedRust/hw-1/src/main.rs
+++ embeddedRust/hw-1/src/main.rs
@@ -2,13 +2,15 @@
 #![no_main]
 
 mod life;
+use core::ops::RangeFrom;
+
 use embedded_hal::{blocking::delay::DelayMs, digital::v2::InputPin};
 use life::*;
 
 use nanorand::Rng;
 
 use cortex_m_rt::entry;
-use microbit::{board::Board, display::blocking::Display, hal::timer::Timer};
+use microbit::{board::Board, display::blocking::Display, hal::timer::Timer, pac::TIMER0};
 
 //use core::time::Duration;
 use panic_rtt_target as _;
@@ -37,24 +39,22 @@ fn init() -> ! {
 
     loop {
         if let Ok(true) = board.buttons.button_a.is_low() {
-            let seed = seeds.next().unwrap();
-            rprintln!("{}", seed);
-            life_board = generate_random_board(&mut life_board, seed);
-            display.show(&mut timer, life_board, 1000);
-            timer.delay_ms(100u16);
-            rprintln!("Pressed 'A' board continues");
-            // print_board(&another_board);
-            // life(&mut another_board);
-            // if done(&another_board) {
-            //     break;
-            // }
-        } else if let Ok(true) = board.buttons.button_b.is_low() {
-            let mut complement_board = [[0; 5]; 5];
-            complement(&mut life_board, &mut complement_board);
-            rprintln!("Pressed 'B' board continues");
-            print_board(&complement_board);
-            display.show(&mut timer, complement_board, 1000);
-            timer.delay_ms(5000u16);
+            handle_button_a(&mut life_board, &mut display, &mut timer, &mut seeds);
+        }
+        // if let Ok(true) = board.buttons.button_a.is_low() {
+        //     let seed = seeds.next().unwrap();
+        //     rprintln!("{}", seed);
+        //     life_board = generate_random_board(&mut life_board, seed);
+        //     display.show(&mut timer, life_board, 1000);
+        //     timer.delay_ms(100u16);
+        //     rprintln!("Pressed 'A' board continues");
+        // print_board(&another_board);
+        // life(&mut another_board);
+        // if done(&another_board) {
+        //     break;
+        // }
+        else if let Ok(true) = board.buttons.button_b.is_low() {
+            handle_button_b(&mut life_board, &mut display, &mut timer);
         } else {
             display.show(&mut timer, life_board, 1000);
             timer.delay_ms(100u16);
@@ -63,15 +63,17 @@ fn init() -> ! {
             life(&mut life_board);
             if done(&life_board) {
                 timer.delay_ms(5000u16);
-                if let Ok(true) = board.buttons.button_a.is_low() {
-                    continue;
-                } else if let Ok(true) = board.buttons.button_b.is_low() {
-                    continue;
-                } else {
-                    let seed = seeds.next().unwrap();
-                    rprintln!("{}", seed);
-                    life_board = generate_random_board(&mut life_board, seed);
-                }
+                // if let Ok(true) = board.buttons.button_a.is_low() {
+                //     rprintln!("Board A continues after Game");
+                //     handle_button_a(&mut life_board, &mut display, &mut timer, &mut seeds);
+                // } else if let Ok(true) = board.buttons.button_b.is_low() {
+                //     rprintln!("Board B continues after Game");
+                //     handle_button_b(&mut life_board, &mut display, &mut timer);
+                // } else {
+                let seed = seeds.next().unwrap();
+                rprintln!("{}", seed);
+                life_board = generate_random_board(&mut life_board, seed);
+                //}
             }
         }
     }
@@ -116,5 +118,36 @@ fn print_board(board_print: &[[u8; 5]]) {
     rprintln!();
 }
 
-// Thing to do is take state of A, and set a flag there that when A is pressed flag becomes active and check state and then pass it to
-// when the button is not pressed to update. THINK!!!!
+fn handle_button_a(
+    life_board: &mut [[u8; 5]; 5],
+    display: &mut Display,
+    timer: &mut Timer<TIMER0>,
+    seeds: &mut RangeFrom<u128>,
+) {
+    let seed = seeds.next().unwrap();
+    rprintln!("{}", seed);
+    *life_board = generate_random_board(life_board, seed);
+    display.show(timer, *life_board, 1000);
+    timer.delay_ms(100u16);
+    rprintln!("Pressed 'A' board continues");
+}
+
+fn handle_button_b(
+    life_board: &mut [[u8; 5]; 5],
+    display: &mut Display,
+    timer: &mut Timer<TIMER0>,
+) {
+    let mut complement_board = [[0; 5]; 5];
+    complement(life_board, &mut complement_board);
+    rprintln!("Pressed 'B' board continues");
+    print_board(&complement_board);
+    display.show(timer, complement_board, 1000);
+    timer.delay_ms(500u16);
+
+    // let seed = seeds.next().unwrap();
+    // rprintln!("{}", seed);
+    // *life_board = generate_random_board(life_board, seed);
+    // display.show(timer, *life_board, 1000);
+    // timer.delay_ms(100u16);
+    // rprintln!("Pressed 'A' board continues");
+}
