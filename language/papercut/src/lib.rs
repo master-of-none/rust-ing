0f5d290fe6e6c7f3e90fc110e9b408fd52655f4d
master-of-none <shrikrishna.bht@iusevimbtw.com>
2026-02-27 22:29:29 +0530

    fe: Initial Commit to A simulation of building a language called `papercut`

---
+++ language/papercut/src/lib.rs
@@ -0,0 +1,18 @@
+#[derive(Debug, PartialEq)]
+pub struct Number(pub i32);
+
+impl Number {
+    pub fn new(s: &str) -> Self {
+        Self(s.parse().unwrap())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_numbers() {
+        assert_eq!(Number::new("123"), Number(123));
+    }
+}
