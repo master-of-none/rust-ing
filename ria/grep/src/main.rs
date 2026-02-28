9a11c20f1888c7c41815363b78a22cf3db6c51c9
master-of-none <shrikrishna.bht@iusevimbtw.com>
2026-02-28 23:09:18 +0530

    fe: Implemented lite version of grep

--- ria/grep/src/main.rs
+++ ria/grep/src/main.rs
@@ -1,15 +1,31 @@
+use clap::{Arg, Command};
+use regex::Regex;
+
 fn main() {
-    let search_term = "picture";
+    let args = Command::new("grep-lite")
+        .version("0.1")
+        .about("Searches for the pattern")
+        .arg(
+            Arg::new("pattern")
+                .help("Pattern to search for")
+                .required(true),
+        )
+        .get_matches();
+
+    let pattern = args.get_one::<String>("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
 
     let quote = "\
-        Every photo, every picture, every face, bedroom window, is a picture in search of what
-        It is same with books.
-        What do we seek, through million of pages.
-        ";
+Every photo, every picture, every face, bedroom window, is a picture in search of what
+It is same with books.
+What do we seek - a picture, through million of pages.";
 
     for line in quote.lines() {
-        if line.contains(search_term) {
-            println!("{}", line);
+        let contains_substring = re.find(line);
+
+        match contains_substring {
+            Some(_) => println!("{}", line),
+            None => (),
         }
     }
 }
